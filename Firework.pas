c570238f838927415ff799d3bfe194a688bef1d4
leduykhongngu <leduykhongngu@gmail.com>
2018-08-04 10:44:41 +0700

    Update Firework.pas

--- Firework.pas
+++ Firework.pas
@@ -34,7 +34,7 @@ constructor TFirework.Create(xx,yy,aa,tt,vv,c:int; mini:boolean);
 begin
         x:=xx; y:=yy;
         x0:=xx; y0:=yy; a:=aa; t:=tt; v:=vv; if (mini) then cl:=c else cl:=pal.colors[c]; IsMini:=mini;
-        lim:=round(v*sin(ToRadian(a))/g);
+        lim:=round(v*sin(ToRadian(a))/g)*3;
 end;
 procedure TFirework.Show();
 begin
@@ -55,13 +55,13 @@ procedure TFirework.Update();
 var     tmp:real;
 begin
         inc(t);
-        tmp:=t/1.5;
+        tmp:=t/3;
         x:=x0+round(v*cos(ToRadian(a))*tmp);
         y:=y0+round(v*sin(ToRadian(a))*tmp-0.5*g*tmp*tmp);
 end;
 function TFirework.IsOutSide():boolean;
 begin
-        exit((x<0) or (y<0) or (x>GetMaxX) or (y>GetMaxY));
+        exit((x<0) or (y<0)) //or (x>GetMaxX) or (y>GetMaxY));
 end;
 function TFirework.IsHighest():boolean;
 begin
@@ -69,7 +69,7 @@ begin
 end;
 function TFirework.CreateMini():TFirework;
 begin
-        result:=TFirework.Create(ran(x-200,x+200),ran(y-200,y+200),ran(a-5,a+5),ran(0,5),30,cl,true);
+        result:=TFirework.Create(x,y,ran(1,360),0,ran(0,30),cl,true);
 end;
 procedure Init();
 var     gd,gm:smallint;
@@ -89,11 +89,11 @@ begin
         tmp:=Tmyvector.create();
         mini:=Tmyvector.create();
         for j:=1 to 5 do
-                tmp.pushback(TFirework.create(ran(10,getmaxx-10),0,ran(70,120),0,ran(75,81),ran(0,250),false));
+                tmp.pushback(TFirework.create(ran(10,getmaxx-10),0,ran(80,100),0,ran(80,85),ran(0,255),false));
         repeat
                 for j:=tmp.size-1 downto 0 do begin;
                         if (tmp[j].IsOutSide) then begin
-                                tmp[j]:=TFirework.create(ran(10,getmaxx-10),0,ran(70,120),0,ran(75,81),ran(0,250),false);
+                                tmp[j]:=TFirework.create(ran(10,getmaxx-10),0,ran(80,100),0,ran(80,85),ran(0,255),false);
                                 continue;
                         end
                         else begin
@@ -104,9 +104,8 @@ begin
                                 if (not tmp[j].IsHighest) then
                                         tmp[j].show()
                                 else begin
-                                        for i:=1 to 300 do begin
+                                        for i:=1 to 150 do begin
                                                 cur:=tmp[j].CreateMini();
-                                                if (sqr(cur.x-tmp[j].x)+sqr(cur.y-tmp[j].y)>40000) then continue;
                                                 mini.pushback(cur);
                                         end;
                                         tmp[j].x:=-1;
@@ -115,7 +114,7 @@ begin
                         end;
                 end;
                 for j:=mini.size-1 downto 0 do begin
-                        if (Mini[j].IsOutSide()) or (Mini[j].t>=20) then begin
+                        if (Mini[j].IsOutSide()) then begin
                                 mini.erase(j);
                                 continue
                         end
@@ -124,9 +123,10 @@ begin
                                 Mini[j].show();
                         end;
                 end;
+                writeln(mini.size()*5);
                 if (mini.size<=0) then
                 delay(30)
-                else  delay(10);
+                else delay(5);
                 clearviewport();
         until keypressed or CloseGraphRequest();
 end;
@@ -138,4 +138,3 @@ begin
         Meo();
         CloseGraph();
 end.
-
